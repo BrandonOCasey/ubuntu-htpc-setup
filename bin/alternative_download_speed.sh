b87fd190eaff91bf8727d820adc31a41b04b018d
Brandon Casey <brandonocasey@gmail.com>
2014-12-20 01:52:27 -0500

    renamed to shorter/better names

--- bin/alternative_download_speed.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-echo "Stopping at $(date)"
-echo "Starting all downloads"
-output="$(transmission-remote --torrent all --start)"
-echo "Setting download limit to 200"
-output="$(transmission-remote --downlimit 200 --uplimit 10)"
-echo "Making torrents start when added"
-output="$(transmission-remote --no-start-paused)"
--- bin/start_download.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-echo "Starting at $(date)"
-echo "Starting all downloads"
-output="$(transmission-remote --torrent all --start)"
-echo "Setting download limit to none"
-output="$(transmission-remote --downlimit 400 --uplimit 40)"
-echo "Making torrents start when added"
-output="$(transmission-remote --no-start-paused)"
